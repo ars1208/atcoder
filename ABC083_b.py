83f1f68a2517007aab7dc0d68c66d8ad280fd093
ars1208 <ara.kaz1208@gmail.com>
2020-03-29 16:02:43 +0900

    fix code

--- ABC083_b.py
+++ ABC083_b.py
@@ -7,11 +7,8 @@ for i in range(n+1):
             all_sum += i
         continue
     i_str = str(i)
-    # print(i)
     i_list = [int(num) for num in i_str]
-    # print(i_list)
     dig_sum = sum(i_list)
-    # print(dig_sum)
     if dig_sum >= a and dig_sum <= b:
         all_sum += i
 
