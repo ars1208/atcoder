2a6a7956c1f0f8313da1c0388232e8212498eb35
ars1208 <ara.kaz1208@gmail.com>
2020-04-02 11:01:32 +0900

    add code

--- ABC085_c.py
+++ ABC085_c.py
@@ -1,1 +1,21 @@
-n, y = map(int,input().split())
+N, Y = map(int,input().split())
+
+flag = False
+for a in range(N+1):
+    for b in range(N+1):
+        if a+b <= N:
+            x = a * 10000
+            y = b * 5000
+            z = (N - a - b) * 1000
+            if x + y + z > Y:
+                continue
+            if x + y + z == Y:
+                print(a,b,N-a-b)
+                flag = True
+                break
+    if flag:
+        break
+
+
+else:
+    print("-1 -1 -1")
