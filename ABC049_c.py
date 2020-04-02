69df192bef99c9ece8bde2fb43f6fdb121ab4c69
ars1208 <ara.kaz1208@gmail.com>
2020-04-02 14:27:15 +0900

    fix code

--- ABC049_c.py
+++ ABC049_c.py
@@ -3,20 +3,12 @@ t = ""
 
 ans = "YES"
 while len(s) > 0:
-    if s[-1] == "m" or s[-1] == "e":
-        if s[-5:] == "dream" or s[-5:] == "erase":
-            s = s[:-5]
-        else:
-            ans = "NO"
-            break
-    elif s[-1] == "r":
-        if s[-7:] == "dreamer":
-            s = s[:-7]
-        elif s[-6:] == "eraser":
-            s = s[:-6]
-        else:
-            ans = "NO"
-            break
+    if s[-5:] == "dream" or s[-5:] == "erase":
+        s = s[:-5]
+    elif s[-7:] == "dreamer":
+        s = s[:-7]
+    elif s[-6:] == "eraser":
+        s = s[:-6]
     else:
         ans = "NO"
         break
