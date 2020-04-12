b2ef4884a5dc0c1bba9086cd08eeb27ba90320b3
ars1208 <ara.kaz1208@gmail.com>
2020-04-13 07:48:39 +0900

    fix code

--- ABC162_c.py
+++ ABC162_c.py
@@ -16,3 +16,19 @@ for a in range(1,k+1):
             sum += gcd(a,b,c)
 
 print(sum)
+
+'''
+def gcd(p,q):
+    if p % q == 0:
+        return q
+    return gcd(q,p%q)
+
+k = int(input())
+sum = 0
+for a in range(1,k+1):
+    for b in range(1,k+1):
+        for c in range(1,k+1):
+            gcd_1 = gcd(max(a,b),min(a,b))
+            sum += gcd(max(gcd_1,c),min(gcd_1,c))
+print(sum)
+'''
